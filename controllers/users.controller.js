0f8102d30381186b15205d36873c659053fbe384
keerti <keerti@gsbitlabs.com.au Logout>
2021-01-29 17:23:44 +0530

    delete, update, verify,add

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -10,17 +10,75 @@ async function createUser(userOpts) {
     if (!userOpts.password) {
         throw new Error('did not supply password')
     }
-    console.log('=========== controller validation done ====================');
     const user = await Users.create(userOpts)
-    console.log('===================after userOpts validation =================');
     if (!user) {
         throw new Error('could not create user')
     }
     return user
 }
+async function verifyUser(userOpts) {
+    if (!userOpts.email) {
+        throw new Error('please enter email')
+    }
+    if (!userOpts.password) {
+        throw new Error('please enter password')
+    }
+    const user = await Users.findOne({
+        where: { email: userOpts.email }
+    })
+    console.log('user===>', user);
+    if (!user) {
+        throw new Error('no user with given email')
+    }
+    if (user.password === userOpts.password) {
+        return user
+    } else {
+        throw new Error('Password dose not match')
+    }
+}
+async function updateUser(userOpts) {
 
+    if (!userOpts.email) {
+        throw new Error('please enter email')
+    }
+    if (!userOpts.password) {
+        throw new Error('please enter password')
+    }
+    if (!userOpts.username) {
+        throw new Error('please enter password')
+    }
+    console.log('=---------------------------validaton dine----------', userOpts.username);
+
+    const user = await Users.update(
+        {
+            username: userOpts.username,
+            password: userOpts.password
+        },
+        {
+            where:
+            {
+                email: userOpts.email
+            }
+        }
+    )
+    return user
+}
+async function deleteUser(userOpts) {
+
+    if (!userOpts.email) {
+        throw new Error('please enter email')
+    }
+    console.log('=---------------------------validaton dine----------', userOpts.username);
+    const user = await Users.destroy({
+        where:
+        {
+            email: userOpts.email
+        }
+    })
+    return user
+}
 module.exports = {
-    createUser
+    createUser, verifyUser, updateUser,deleteUser
 }
 
 // const { Users } = require('../models')
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const { createUser } = require('../../controllers/users.controller')
+const { verifyUser } = require('../../controllers/users.controller')
+const { updateUser } = require('../../controllers/users.controller')
+const { deleteUser } = require('../../controllers/users.controller')
+
+
 
 router.post('/', async (req, res) => {
     console.log('============================================');
@@ -15,6 +20,49 @@ router.post('/', async (req, res) => {
         email: req.body.email
     })
     res.send(user)
+})
+router.post('/login', async (req, res) => {
+    try {
+        console.log('req.body.user:', req.body)
+        const verify = await verifyUser(req.body)
+        res.send(verify)
+    } catch (err) {
+        res.json({
+            success: false,
+            error: err
+        })
+    }
+
+})
+router.post('/update', async (req, res) => {
+    try {
+        console.log('req.body.user:', req.body)
+        const update = await updateUser(req.body)
+        res.json({
+            success: true,
+            message: "update successfully"
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            error: err
+        })
+    }
+})
+router.post('/delete', async (req, res) => {
+    try {
+        console.log('req.body:', req.body)
+        const update = await deleteUser(req.body)
+        res.json({
+            success: true,
+            message: "delete successfully"
+        })
+    } catch (err) {
+        res.json({
+            success: false,
+            error: err
+        })
+    }
 })
 
 module.exports = router;
